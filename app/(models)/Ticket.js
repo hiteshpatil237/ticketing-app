8cff8854d1863be5a78d26ce8c6e6dcf3d3084c1
hiteshpatil237 <hiteshpatil237@gmail.com>
2023-10-25 14:57:50 -0400

    Started with the DB creation using MongoDB and secured connection

---
+++ app/(models)/Ticket.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema, mongo } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
+export default Ticket;
