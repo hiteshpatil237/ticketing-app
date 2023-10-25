96869f18cdc6a50c2d423796c69343a49988af8b
hiteshpatil237 <hiteshpatil237@gmail.com>
2023-10-25 13:15:46 -0400

    Created the Navbar and added styles to the entire system

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,11 @@ import Nav from "./(components)/Nav";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,8 +18,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Nav /> 
-        {children}
+        <div className="flex flex-col h-screen max-h-screen">
+          <Nav />
+          <div className="flex-grow overflow-y-auto bg-page text-default-text">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
