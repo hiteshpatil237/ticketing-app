1b636ed68e6fa0d227b65c907d13fba4bc921c63
hiteshpatil237 <hiteshpatil237@gmail.com>
2023-10-25 14:20:26 -0400

    Completed the styling of the actual components

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -5,11 +5,28 @@ import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
-      <ProgressDisplay />
-      <StatusDisplay />
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">
+        This is the ticket description! Please do this ticket
+      </p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">10/23/2023 14:14pm</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
